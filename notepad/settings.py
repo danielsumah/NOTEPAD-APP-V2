d9b66ed51774a1862a88dc669ca57848888a4ed5
Daniel Sumah <danielsumah@gmail.com>
2021-05-05 14:25:36 +0100

    app set up for heroku deployment

--- notepad/settings.py
+++ notepad/settings.py
@@ -12,9 +12,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-!%i0y&ovih)xi$8z@sn_0skiw#^^gddoqfp@&g5ei!rtgih&y3'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['sumah-todo-app.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
